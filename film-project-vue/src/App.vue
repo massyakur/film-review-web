721b5aafe99758cd990c90a530addf17af1a28d7
massyakur <200201147@kocaeli.edu.tr>
2021-12-13 20:34:38 +0300

    Icons changed

--- film-project-vue/src/App.vue
+++ film-project-vue/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <!-- App.vue -->
 
-<v-app>
-  <v-navigation-drawer app v-model="drawer">
-    <!--side bar -->
-    <v-list>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer">
+      <!--side bar -->
+      <v-list>
         <v-list-item v-if="!guest">
           <v-list-item-avatar>
             <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
@@ -14,7 +14,7 @@
           </v-list-item-content>
         </v-list-item>
 
-        <div class="pa-2" v-if="guest"> 
+        <div class="pa-2" v-if="guest">
           <v-btn block color="primary" class="mb-1">
             <v-icon left>mdi-lock</v-icon>
             Login
@@ -27,78 +27,70 @@
 
         <v-divider></v-divider>
 
-        <v-list-item 
-          v-for="(item, index) in menus"
-          :key="`menu-${index}`"
-          :to="item.route"
-        >
+        <v-list-item v-for="(item, index) in menus" :key="`menu-${index}`" :to="item.route">
           <v-list-item-icon>
-            <v-icon left>{{item.icon}}</v-icon>
+            <v-icon left>{{ item.icon }}</v-icon>
           </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title>{{item.title}}</v-list-item-title>           
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
-        </v-list-item>   
+        </v-list-item>
+      </v-list>
 
-    </v-list>
-
-    <template v-slot:append v-if="!guest">
-      <div class="pa-2"> 
+      <template v-slot:append v-if="!guest">
+        <div class="pa-2">
           <v-btn block color="red" dark>
             <v-icon left>mdi-lock</v-icon>
             Logout
-          </v-btn>          
+          </v-btn>
         </div>
-    </template>
+      </template>
 
       <v-divider></v-divider>
-  </v-navigation-drawer>
+    </v-navigation-drawer>
 
-  <v-app-bar app color="success" dark>
-    <!-- -->
-    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-    <v-toolbar-title>Kelompok-6Apps</v-toolbar-title>
+    <v-app-bar app color="success" dark>
+      <!-- -->
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Kelompok-6Apps</v-toolbar-title>
 
-    <!-- pemisah konten -->
-    <v-spacer></v-spacer>
+      <!-- pemisah konten -->
+      <v-spacer></v-spacer>
+    </v-app-bar>
 
-  </v-app-bar>
-
-  <!-- Sizes your content based upon application components -->
-  <v-main>
-
-    <!-- Provides the application the proper gutter -->
-    <v-container fluid>
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
         <v-slide-y-transition>
-            <!-- If using vue-router -->
-            <router-view></router-view>
-        </v-slide-y-transition>      
-    </v-container>
-  </v-main>
+          <!-- If using vue-router -->
+          <router-view></router-view>
+        </v-slide-y-transition>
+      </v-container>
+    </v-main>
 
-  <v-footer app>
-    <!--footer-->
-    @Kelompok-6 | VueJS
-  </v-footer>
-</v-app>
+    <v-footer app>
+      <!--footer-->
+      @Kelompok-6 | Laravel & VueJS
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
     //
-    drawer : true,
-    menus : [
-      { title : 'Home', icon : 'mdi-home', route : '/'},
-      { title : 'Film', icon : 'mdi-note', route : '/Film'},
-      { title : 'Cast', icon : 'mdi-note', route : '/Cast'},
-      { title : 'Genre', icon : 'mdi-note', route : '/Genre'}
+    drawer: true,
+    menus: [
+      { title: "Home", icon: "mdi-home", route: "/" },
+      { title: "Film", icon: "mdi-theater", route: "/Film" },
+      { title: "Cast", icon: "mdi-account-group", route: "/Cast" },
+      { title: "Genre", icon: "mdi-drama-masks", route: "/Genre" },
     ],
-    guest : true
+    guest: true,
   }),
 };
 </script>
